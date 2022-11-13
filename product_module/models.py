7484ce96aa0c4e7f610507e06c1e6b8f7a1494f1
ne3mer <ne3mer@gmail.com>
2022-11-13 15:17:08 +0330

    first commit django product model

---
+++ product_module/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=300)
+    body = models.TextField()
+    designer = models.CharField(max_length=300)
